6a43b135631195d847965c9ca2ed4304d6124260
Alessandre Livramento <aleliv74@gmail.com>
2022-03-04 17:58:11 -0300

    Chapter V - Primeiro teste/Seletores

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,4 +3,28 @@ import { render } from '@testing-library/react-native'
 
 import { Profile } from '../../screens/Profile'
 
-test('check if show correctly user input name placeholder', () => {})
+test('check if show correctly user input name placeholder', () => {
+  const { getByPlaceholderText } = render(<Profile />)
+
+  const inputName = getByPlaceholderText('Nome')
+
+  expect(inputName).toBeTruthy()
+})
+
+test('checks if user data has been loaded', () => {
+  const { getByTestId } = render(<Profile />)
+
+  const inputName = getByTestId('input-name')
+  const inputSurname = getByTestId('input-surname')
+
+  expect(inputName.props.value).toEqual('Alessandre')
+  expect(inputSurname.props.value).toEqual('Livramento')
+})
+
+test('checks if title render correctly', () => {
+  const { getByTestId } = render(<Profile />)
+
+  const textTitle = getByTestId('text-title')
+
+  expect(textTitle.props.children).toContain('Perfil')
+})
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -4,10 +4,10 @@ import { View, Text, TextInput, Button } from 'react-native'
 export function Profile() {
   return (
     <View>
-      <Text>Perfil</Text>
+      <Text testID="text-title">Perfil</Text>
 
-      <TextInput placeholder="Nome" autoCorrect={false} />
-      <TextInput placeholder="Sobrenome" />
+      <TextInput testID="input-name" placeholder="Nome" autoCorrect={false} value="Alessandre" />
+      <TextInput testID="input-surname" placeholder="Sobrenome" value="Livramento" />
       <Button title="Salvar" onPress={() => {}} />
     </View>
   )
