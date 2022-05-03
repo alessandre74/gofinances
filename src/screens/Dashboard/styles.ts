88bba4643266151a8b576c8d2faa27b47db25110
Alessandre Livramento <aleliv74@gmail.com>
2022-05-03 01:15:25 -0300

    Alteração Flatlist arquivo Dashboard styles

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components/native'
-import { FlatList } from 'react-native'
+import { FlatList, FlatListProps } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
 import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper'
@@ -98,7 +98,9 @@ export const Title = styled.Text`
     margin-bottom: 16px;
   `}
 `
-export const TransactionList = styled(FlatList as new () => FlatList<DataListProps>).attrs({
+export const TransactionList = styled(
+  FlatList as new (props: FlatListProps<DataListProps>) => FlatList<DataListProps>
+).attrs({
   showsVerticalScrollIndicator: false,
   contentContainerStyle: { paddingBottom: getBottomSpace() }
 })``
